cc6dd7bafa748f43f555108096da7d37ae8264b6
mateusvascaino <163007807+mateusvascaino@users.noreply.github.com>
2024-10-21 11:33:14 -0300

    yfu

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    const palpites = document.querySelector('.palpites');
+    const ultimoResultado = document.querySelector('.ultimoResultado');
+    const baixoOuAlto = document.querySelector('.baixoOuAlto');
+    const envioPalpite = document.querySelector('.envioPalpite');
+    const campoPalpite = document.querySelector('.campoPalpite');
+    let contagemPalpites = 1;
+    let botaoReiniciar;
 
+    function verificarPalpite() {
+        const palpiteUsuario = Number(campoPalpite.value);
+        if (contagemPalpites === 1) {
+          palpites.textContent = "Palpites anteriores: ";
+}
+
+    palpites.textContent += palpiteUsuario + " ";
+
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = "Parabéns! Você acertou!";
+        ultimoResultado.style.backgroudColor = "green";
+        baixoOuAlto.textContent = "";
+        finalizarjogo();
+    } else if (contagemPalpites === 10) {
+        ultimoResultado.textContent = "FIM DE JOGO!!!";
+        baixoOuAlto.textContent = "";
+        finalizarjogo();
+    } else {
+        ultimoResultado.textContent = "Errado";
+        ultimoResultado.style.backgroudColor = "red";
+        if (palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = "O ultimo palpite foi muito baixo";
+    } else if (palpiteUsuario > numeroAleatorio() {
+      baixoOuAlto.textContent =  "O ultimo palpite foi muito alto";
+    }
+} 
