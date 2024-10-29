aeb985b6d1a3eb19326ccb6f21dbf8a09002029b
mateusvascaino <163007807+mateusvascaino@users.noreply.github.com>
2024-10-29 08:37:12 -0300

    uih

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function finalizarjogo() {
     envioPalpite.disable = true;
     botaoReiniciar = document.createElement('button');
     document.body.appendChild(botaoReiniciar);
-    botaoReiniciar.textContent = "Reiniciar Jogar";
+    botaoReiniciar.textContent = "Reiniciar Jogo";
     botaoReiniciar.classList.add('botaoReiniciar');
     document.body.appendChild(botaoReiniciar);
     botaoReiniciar.addEventListener('click', reiniciarJogo);
